2fede0c7dc0730f19c0631a59ebd316969d03084
divyansh0x0 <divyansh0x0@gmail.com>
2026-07-25 12:13:52 +0530

    feat: fix drawing of linkage

--- scripts/linkage/Link.py
+++ scripts/linkage/Link.py
@@ -1,3 +1,6 @@
+import math
+
+import numpy as np
 import pygame.draw
 
 from linkage import PhysicsObject
@@ -7,9 +10,21 @@ class Link(PhysicsObject.PhysicsObject):
     def __init__(self, length):
         super().__init__()
         self.length = length
-
+    def __getPoints(self):
+        start_x, startY = self.position
+        end_x = start_x + self.length * math.cos(self.angular_position * math.pi / 180)
+        end_y = startY + self.length * math.sin(self.angular_position * math.pi / 180)
+        return start_x, startY, end_x, end_y
     def update(self):
         super().update()
     def draw(self, dt, surface) -> None:
+        start_x, start_y, end_x, end_y = self.__getPoints()
+        pygame.draw.line(surface, (0,0,0), (start_x, start_y), (end_x, end_y), 2)
 
-        pygame.draw.rect(surface, (0,0,0), [10,10,10,10])
+    def fixStart(self):
+        self.velocity = np.array([0.0, 0.0])
+    def fixEnd(self):
+        self.velocity = np.array([0.0, 0.0])
+        start_x, start_y, end_x, end_y = self.__getPoints()
+        self.position = np.array([end_x, end_y])
+        self.angular_position -= 180.0
--- scripts/linkage/PhysicsObject.py
+++ scripts/linkage/PhysicsObject.py
@@ -1,16 +1,15 @@
 import numpy as np
 
-
 class PhysicsObject:
     def __init__(self):
-        self.position = np.array([0.0, 0.0])
+        self.position = np.array([5.0, 50.0])
         self.orientation = np.array([0.0, 0.0])
-        self.velocity = np.array([0.0, 0.0])
+        self.velocity = np.array([2.0, 0.0])
         self.linear_acceleration = np.array([0.0, 0.0])
-        self.angular_position = 0.0
-        self.angular_velocity = 0.0
+        self.angular_position = -90.0
+        self.angular_velocity = 10.0
         self.angular_acceleration = 0.0
-        self.dt = 1000/120
+        self.dt = 1/120
         self.mass = 1.0
 
     def update(self):
